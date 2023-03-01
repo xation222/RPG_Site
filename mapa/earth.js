b01acd632f4543e0adc2b0a029150caeb329f2ed
xation222 <126095360+xation222@users.noreply.github.com>
2023-03-01 14:27:20 -0300

    baguio que segue

--- mapa/earth.js
+++ mapa/earth.js
@@ -1,11 +1,9 @@
-        
-        document.addEventListener('mousemove', function(e) {  //seguir o mouse
-            let body = document.querySelector('body');
+        document.addEventListener('mousemove', function(e) {
             let circle = document.getElementById('circle');
-            let left = e.offsetX;
-            let top = e.offsetY;
+            let left = e.pageX;
+            let top = e.pageY;
             circle.style.left = left + 'px';
             circle.style.top = top + 'px';
         })
-        
-        // document.body.style.cursor = 'none';  //esconder cursor do mouse
+
+        //document.body.style.cursor = 'none';  //esconder cursor do mouse
