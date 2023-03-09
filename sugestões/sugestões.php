2cde9ce6d012f3d59de8bc434b93ce0ee4538a4c
xation222 <126095360+xation222@users.noreply.github.com>
2023-03-09 14:59:18 -0300

    sugestões

---
+++ sugestões/sugestões.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Obrigado!</title>
+</head>
+<body>
+    <h1>Obrigado por nos ajudar!</h1>
+    <?php 
+        date_default_timezone_set("America/Sao_Paulo");
+        $momento = date("d/M/Y G:i:s");
+        $entrada = $_POST["sugestões"];
+
+        $reg = fopen("sugestões.txt", "a");
+        fwrite($reg, "\n$momento\n$entrada");
+        fclose($reg);
+    ?>
+</body>
+</html>
